8e494de4a79cb6320ee9cbdd6820e4a2acd2dfd8
Thomas Tsoi <tsoithomas@outlook.com>
2023-08-07 19:29:01 -0400

    Improved responsive design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ const Header = () => {
 
     return (
         <div className="sticky top-0 z-50">
-            <header className="w-full flex flex-row bg-rose-900 text-white px-5 py-4 text-gray-300 place-items-center">
-                <img src={logo} className="h-12 logo" />
-                <h1 className="overflow-hidden transition-all whitespace-nowrap text-2xl sm:text-3xl font-bold ms-4 grow">
+            <header className="w-full flex flex-row bg-rose-900 text-white px-4 py-2 sm:py-4 text-gray-300 place-items-center transition-all">
+                <img src={logo} className="logo h-8 w-8 sm:h-12 sm:w-12"/>
+                <h1 className="overflow-hidden transition-all whitespace-nowrap text-2xl sm:text-3xl ms-2 sm:ms-4 font-bold ms-4 grow">
                     <Link to={`/`}>Charity Finder</Link>
                 </h1>
             </header>
