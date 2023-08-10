b93a458eb9b2522bebfa9110c0c13fa10e00d9c6
Thomas Tsoi <tsoithomas@outlook.com>
2023-08-10 00:03:58 -0400

    Improved list design

--- src/components/CharityList.tsx
+++ src/components/CharityList.tsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import locationIcon from "../assets/location.svg"
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 interface CharityListProp {
     charities: Charity[]
 }
 
 
-
-const CharityList = (props: CharityListProp) => {
-
+const CharityList = (props: CharityListProp) => {    
     return (
         
         <div className="w-full bg-amber-50 grow p-4">
+            <div className="max-w-4xl mx-auto flex flex-row flex-wrap items-stretch">
 
             {
                 props.charities.map((item) => {
                     return (
-                        <Link to={"/charity/"+item.ein} className="block w-full max-w-3xl mx-auto" key={item.ein}>
-                            <section className="border border-gray-400 bg-white hover:bg-amber-200 transition-all rounded-lg m-2 px-6 py-4 flex flex-col justify-between leading-normal">
+                        <Link to={"/charity/"+item.ein} className="inline-block w-full sm:w-[18.5rem] mb-6" key={item.ein}>
+                            <section className="border border-gray-400 h-full bg-white hover:bg-amber-200 transition-all rounded-lg flex flex-col leading-normal shadow m-2 overflow-hidden">
+                                <div className="block w-full h-28 bg-gray-200 overflow-hidden border-b">
+                                <img src={item.coverImageUrl} className="object-cover w-full h-28" />
+                                </div>
+                                <div className="px-6 py-4 ">
                                 <h3 className="font-bold overflow-hidden text-md text-gray-700">{item.name}</h3>
                                 <div className="overflow-hidden flex flex-row">
                                     <span><img src={locationIcon} className="h-4 mt-0.5 me-1" /></span>
@@ -28,6 +32,7 @@ const CharityList = (props: CharityListProp) => {
                                     item.description &&
                                     <div className="overflow-hidden text-gray-700 mt-2 text-sm">{item.description}</div>
                                 }
+                                </div>
                             </section>
                         </Link>
                     )
@@ -36,8 +41,7 @@ const CharityList = (props: CharityListProp) => {
 
             }
 
-
-
+            </div>
         </div>
     )
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,48 +13,65 @@ function Home() {
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
+      const keywords = ["charity", "pet", "women", "immigrant", "poverty", "refugee", "environment", "children"];
+      performSearch(keywords[Math.floor(Math.random()*keywords.length)]);
+
       const onScroll = () => {
         setOffset(window.scrollY);
       }
-
       // clean up code
       window.removeEventListener('scroll', onScroll);
       window.addEventListener('scroll', onScroll, { passive: true });
+
       return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
-  
-
-  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const searchTerm = (e.target as HTMLFormElement).search.value;
+    performSearch(searchTerm);
+  }
 
+  const performSearch = async (searchTerm: string) => {
     try {
-      const data = await axios.get(searchUri + searchTerm + "?apiKey=" + apiKey);
+      const data = await axios.get(searchUri + searchTerm + "?take=21&apiKey=" + apiKey);
 
       let records: Charity[] = [];
 
       for (const item of data.data.nonprofits) {
-        const charityRecord = {
-          ein: item["ein"],
-          name: item["name"],
-          description: item["description"],
-          location: item["location"],
-          profileUrl: item["profileUrl"],
+        if (typeof item.ein !== 'undefined') {
+          // const detailData = await axios.get(detailUri + item["ein"] + "?apiKey=" + apiKey);
+          // const org = detailData.data.data.nonprofit;
+
+
+          const charityRecord = {
+            ein: item.ein,
+            name: item.name,
+            description: item.description,
+            location: item.location,
+            profileUrl: item.profileUrl,
+            coverImageUrl: item.coverImageUrl,
+            // descriptionLong: org.descriptionLong,
+            // logoUrl: org.logoUrl,
+            // websiteUrl: org.websiteUrl,
+          }
+
+          records.push(charityRecord);
         }
-        records.push(charityRecord);
       }
 
       setCharities(records)
       
-      console.log(data);
+      // console.log(data);
 
     }
     catch (error) {
       console.log(error)
     }
-
   }
+
+
+
 
   return (
     <div className="flex flex-col min-h-screen">
